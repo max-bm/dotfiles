c0d65f93c8807231efc025b7610e830dda4e43ed
Max Buckmire-Monro <max.buckmire-monro@cts.co>
2023-05-24 23:14:46 +0100

    Neovim appimage install. .local perm change.

--- devcontainer_setup.sh
+++ devcontainer_setup.sh
@@ -1,4 +1,12 @@
 #! /bin/zsh
 sudo apt-get update
-sudo apt-get install -y vim
-sudo apt-get install -y neovim
+sudo apt-get install -y vim fuse
+# Install neovim AppImage
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+
+sudo chown -R $1:$1 .local
